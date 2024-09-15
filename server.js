897df6395fadc087b3518f1d81e621fd1a4581dc
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2024-09-15 19:53:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ import AuthRoute from "../lead/Route/AuthRoute.js"
 import TaskRoute from "../lead/Route/TaskRoute.js"
 import msgRoute from './Route/MsgRoute.js'
 import cronJobs from "./Schedule/cronJobs.js";
+import path from 'path'
+import { fileURLToPath } from "url";
 configDotenv()
 ConnectDb()
+//02-hosting k liye(start)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 const app = express() 
 app.use(express.json())
 app.use(cors())
@@ -15,6 +20,9 @@ app.use(cors())
 app.use("/api/v1/auth",AuthRoute) 
 app.use("/api/v1/Task",TaskRoute) 
 app.use("/api/v1/msg",msgRoute)
+
+//03-hoisting(start)
+app.use(express.static(path.join(__dirname, './client/build')))
 
 const Port = process.env.PORT
 app.listen(Port,()=>{
